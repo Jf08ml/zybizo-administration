c8c9946dd02ee81cac5663a64ab2b3eadd408d04
juanfe <lassojuanfe@gmail.com>
2024-06-15 19:17:21 -0500

    Añade google analytics

---
+++ frontend/src/boot/gtag.js
@@ -0,0 +1,12 @@
+import { boot } from 'quasar/wrappers'
+import { createGtag } from 'vue-gtag-next'
+
+export default boot(({ app }) => {
+  const gtag = createGtag({
+    property: {
+      id: 'G-7VW8DTFWVX'
+    }
+  })
+  
+  app.use(gtag)
+})
