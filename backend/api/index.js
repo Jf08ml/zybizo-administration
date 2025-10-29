fc3f9645d6eca6c57fb03a1d3828b16cfbb0f5dd
juanfe <lassojuanfe@gmail.com>
2025-10-29 15:15:00 -0500

    añade alls.js

--- backend/api/index.js
+++
@@ -1,8 +0,0 @@
-// backend/api/[...all].js
-import app from "../src/app.js";
-
-// Vercel (Node 20) necesita un handler (req, res).
-// Express "app" ya es un manejador compatible.
-export default function handler(req, res) {
-  return app(req, res);
-}
